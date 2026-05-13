515a3bb0fb0afd633b747c5187586b13fb1dcb05
j <castro.jaime@proton.me>
2026-05-14 01:17:06 +0200

    fix: better memmove

--- src/mem/hugebranches/ft_memmove_huge.c
+++ src/mem/hugebranches/ft_memmove_huge.c
@@ -6,7 +6,7 @@
 /*   By: jaicastr <jaicastr@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 00:05:58 by jaicastr          #+#    #+#             */
-/*   Updated: 2026/05/14 01:02:03 by jaicastr         ###   ########.fr       */
+/*   Updated: 2026/05/14 01:16:38 by jaicastr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,11 +39,10 @@ inline void	ft_memmove_512_huge(void *restrict dest,
 	void		*s;
 
 	d = ft_align_bkw((t_blk8w)dest + n, 64);
-	s = (t_blk8w)src + n;
+	s = (t_blk8w)src + (n - ((t_uptr)d - (t_uptr)dest));
 	*(t_blk512w)ft_overlap(dest, sizeof(t_vu512a), n) =
 		*(t_blk512r)ft_overlap(src, sizeof(t_vu512a), n);
-	n -= sizeof(t_vu512a);
-	blkidx = n >> 6;
+	blkidx = (n - sizeof(t_vu512a)) >> 6;
 	while (blkidx >= 4)
 	{
 		x[0] = ((t_blk512r)s)[blkidx];
